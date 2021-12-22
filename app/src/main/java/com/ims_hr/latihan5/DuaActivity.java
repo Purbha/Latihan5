c7acca771811b655373e18ab212a04d35b55e123
Irsyad Purbha <irsyadpurbha@gmail.com>
2021-12-22 21:21:36 +0700

    UPDATE 20211222 21:07

--- app/src/main/java/com/ims_hr/latihan5/DuaActivity.java
+++ app/src/main/java/com/ims_hr/latihan5/DuaActivity.java
@@ -1,7 +1,8 @@
 package com.ims_hr.latihan5;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class DuaActivity extends AppCompatActivity {
 
--- app/src/main/java/com/ims_hr/latihan5/MainActivity.java
+++ app/src/main/java/com/ims_hr/latihan5/MainActivity.java
@@ -2,8 +2,9 @@ package com.ims_hr.latihan5;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
